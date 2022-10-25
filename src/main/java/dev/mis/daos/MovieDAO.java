f8195a9752571b087e5c3b6b880f0b7f1ad62079
Griffy <gchamber@alumni.usc.edu>
2022-10-25 11:12:16 -0700

    finishing MovieDAO

--- src/main/java/dev/mis/daos/MovieDAO.java
+++ src/main/java/dev/mis/daos/MovieDAO.java
@@ -31,9 +31,66 @@ public class MovieDAO {
 
         }
     }
-    public Movie getMovieById(int movieID){}
-    //should check DB first, and catch SQL exception by sending movieID prefaced with "tt" to OMDB API
-    public boolean updateMovie(Movie movie){}
+    public Movie getMovieById(int movieID){
+        try(Connection conn = ConnectionUtil.createConnection()){
+        String sql = "select * from mis.movies where movie_id=?";
+        PreparedStatement ps =conn.prepareStatement(sql);
+        ps.setInt(1,movieID);
+        ResultSet rs =ps.executeQuery();
+        if(rs.next()){
+            Movie movie = new Movie(
+                    rs.getInt("movie_id"),
+                    rs.getString("title"),
+                    rs.getFloat("IMDB_rating"),
+                    rs.getString("original_language"),
+                    rs.getInt("release_year")
+            );
+            return movie;
+        }else{
+            return null;
+        }
 
-    public boolean deleteMovie(int movieID){}
+    }
+    catch (SQLException e){
+        e.printStackTrace();
+        return null;
+
+    }}
+    //should check DB first, and catch SQL exception by sending movieID prefaced with "tt" to OMDB API
+    public boolean updateMovie(Movie movie){
+        try(Connection conn = ConnectionUtil.createConnection()){
+            String sql = "update mis.movies set title=?, IMDB_rating=?, original_language=?, release_year=? where movie_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,movie.getTitle());
+            ps.setFloat(2, movie.getRating());
+            ps.setString(3,movie.getLanguage());
+            ps.setInt(4,movie.getReleaseYear());
+            ps.setInt(5,movie.getMovieID());
+            int rowCount = ps.executeUpdate();
+            if (rowCount==1){
+                return true;
+            }else{
+                return false;}
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteMovie(int movieID){
+        try (Connection connection = ConnectionUtil.createConnection()) {
+            String sql = "delete from mis.movies where movie_id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, movieID);
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
