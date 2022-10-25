1758b4c7e365e1e2e5b9fb8340ed648bda33d030
Aheck105 <HeretoHeckel@outlook.com>
2022-10-25 10:20:20 -0400

    moviesforusers to LibraryEntry

---
+++ src/main/java/dev/mis/services/LibraryEntryService.java
@@ -0,0 +1,16 @@
+package dev.mis.services;
+
+import dev.mis.daos.LibraryEntryDAO;
+import dev.mis.daos.MovieDAO;
+import dev.mis.daos.UserDAO;
+import dev.mis.entities.LibraryEntry;
+
+public class LibraryEntryService {
+
+    private LibraryEntryDAO libraryEntryDAO;
+
+    public LibraryEntryService(UserDAO, MovieDAO){
+
+    }
+
+}
