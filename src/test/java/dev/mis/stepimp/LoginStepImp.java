67eabab9cba841239738ebda1a0e47d936c19b99
lemonadedance <sopphey@gmail.com>
2022-10-27 15:14:09 -0500

    checked login e2e test

--- src/test/java/dev/mis/stepimp/LoginStepImp.java
+++ src/test/java/dev/mis/stepimp/LoginStepImp.java
@@ -5,7 +5,6 @@ import dev.mis.runner.TestRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
