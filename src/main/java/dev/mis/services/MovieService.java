3b306dc4eac8f661d1a5798a37a91dd4d7911515
Griffy <gchamber@alumni.usc.edu>
2022-10-26 22:39:19 -0700

    roadblock on OMDB API calls, potential scope change. Minor refactoring of Movie object

--- src/main/java/dev/mis/services/MovieService.java
+++ src/main/java/dev/mis/services/MovieService.java
@@ -1,5 +1,8 @@
 package dev.mis.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.*;
 import com.google.gson.JsonArray;
 import dev.mis.daos.MovieDAO;
@@ -29,10 +32,10 @@ public class MovieService {
 
 
 
-    public Movie searchMovieById(String movieID) throws ExecutionException, InterruptedException {
+    public Movie searchMovieById(String movieID) throws ExecutionException, InterruptedException, JsonProcessingException {
         Movie movie =  movieDAO.getMovieById(movieID);
         if (movie != null){return movie;}else{
-            String uri = "http://www.omdbapi.com/?i="+movieID+"&apikey=[9c0cebf2]";
+           String uri = "http://www.omdbapi.com/?i="+movieID+"&apikey=[9c0cebf2]";
            HttpClient client = HttpClient.newHttpClient();
            HttpRequest request = HttpRequest.newBuilder(
                    URI.create(uri))
@@ -40,9 +43,15 @@ public class MovieService {
            CompletableFuture<HttpResponse<String>> res = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
            HttpResponse<String> response = res.get();
            Gson gson = new Gson();
-           String jsonRes =gson.toJson(res);
-            String[] movieKeys = response.toString().split(",:",0);
-            Movie newMovie = new Movie(movieID,movieKeys[1],movieKeys[1],movieKeys[1],Integer.parseInt(movieKeys[3]));
+           String jsonRes =gson.toJson(response);
+           ObjectMapper map = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+           Movie newMovie = map.readValue(jsonRes,Movie.class);
+           newMovie.setMovieID(movieID);
+           //this WILL work, provided Movie class variable names are renamed. Alternatives: make new OMDBMovie class
+            //Getting Ratings to work will also be difficult as it is stored as separate object
+            //Perhaps parsing as String and getting creative with regex .split() is the way
+            //String[] movieKeys = response.toString().split(",:",0);
+            //Movie newMovie = new Movie(movieID,movieKeys[1],movieKeys[1],movieKeys[1],Integer.parseInt(movieKeys[3]));
 
            System.out.println(response);
            //if(res == null){return null;}
@@ -52,11 +61,5 @@ public class MovieService {
         }
 
     //this is the Dynamic Search mentioned in business requirements
-}
-
-    public static void main(String[] args) {
-        MovieDAO movieDAO1 = new MovieDAO();
-        MovieService movieService = new MovieService(movieDAO1);
-        movieService.searchMovieById("tt5108870");
     }
 }
