760676aba799aa47f3798bfc1ebfe21b428d8cc7
lemonadedance <sopphey@gmail.com>
2022-10-27 20:05:23 -0500

    added pseudocode to Library E2E Tests

--- src/test/java/dev/mis/stepimp/LibraryAddStepImp.java
+++ src/test/java/dev/mis/stepimp/LibraryAddStepImp.java
@@ -8,43 +8,62 @@ public class LibraryAddStepImp {
 
     @Given("the user is logged in and on their {string} page")
     public void the_user_is_logged_in_and_on_their_page(String string) {
-        // Write code here that turns the phrase above into concrete actions
+
+        //get login
+        //find usernameInput
+        //sendkeys
+        //find userpassInput
+        //sendkeys
+        //click login
+        //explicit wait for "Library View"
 
     }
 
     @When("the user enters {string} into the IMDB ID field")
     public void the_user_enters_into_the_imdb_id_field(String string) {
-        // Write code here that turns the phrase above into concrete actions
+
+        //findIMDBID Field
+        //sendkeys
 
     }
 
     @When("the user clicks the Get Movie Information button")
     public void the_user_clicks_the_get_movie_information_button() {
-        // Write code here that turns the phrase above into concrete actions
+
+        //findGetMovieInformationButton
+        //click
 
     }
 
     @Then("the movie's information should be input into the correct fields")
     public void the_movie_s_information_should_be_input_into_the_correct_fields() {
-        // Write code here that turns the phrase above into concrete actions
+
+        //wait
+        //assert movie information was populated
 
     }
 
     @When("the user checks the box for Watched")
     public void the_user_checks_the_box_for_watched() {
-        // Write code here that turns the phrase above into concrete actions
+
+        //find watchedcheckbox
+        //click watched checkbox
 
     }
 
     @When("the user clicks the Submit button")
     public void the_user_clicks_the_submit_button() {
-        // Write code here that turns the phrase above into concrete actions
+
+        //find submitButton
+        //click submit button
 
     }
 
     @Then("the new entry should appear on the user's Library View")
     public void the_new_entry_should_appear_on_the_user_s_library_view() {
-        // Write code here that turns the phrase above into concrete actions
+
+        //wait for new entry to appear
+        //assert new entry is displayed
 
     }
 }
