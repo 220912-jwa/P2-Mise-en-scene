59c8a92fbcd74f09f1c9d7daac22c5c93dec6cbf
Aheck105 <HeretoHeckel@outlook.com>
2022-11-01 11:35:15 -0400

    user code generation added in userservice

--- src/main/java/dev/mis/services/UserService.java
+++ src/main/java/dev/mis/services/UserService.java
@@ -3,13 +3,15 @@ package dev.mis.services;
 import dev.mis.daos.MovieDAO;
 import dev.mis.daos.UserDAO;
 import dev.mis.entities.User;
+import org.joda.time.DateTime;
 
 import java.util.List;
-
+import java.util.Random;
 public class UserService {
     private UserDAO userDAO;
     private MovieDAO movieDAO;
 
+    private static final String CHAR_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public UserService(UserDAO userDAO, MovieDAO movieDAO) {
 
@@ -20,6 +22,30 @@ public class UserService {
     public MovieDAO getMovieDAO(int id) {
 
         return movieDAO.getById(id);
+    }
+
+    public String generateUserCode(){
+        StringBuilder code = new StringBuilder();
+
+        int n = 0;
+        for(int i=0; i < 10; i++){
+            n = getRandomNumber();
+            char ch = CHAR_LIST.charAt(n);
+            code.append(ch);
+        }
+        return code.toString();
+    }
+
+    private int getRandomNumber(){
+        int random = 0;
+        Random randomGenerator = new Random();
+        random = randomGenerator.nextInt(CHAR_LIST.length());
+        if(random - 1 == -1){
+            return random;
+        }
+        else{
+            return random - 1;
+        }
     }
 
     public List<User> getAllUsers() {
